7b7a130d17284dcf3ab1cad8554c42922a8e8055
andreap <andreaparaggio@gmail.com>
2021-12-23 11:50:59 +0100

    Initial Commit

---
+++ Shared/VisionContourDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionContourDetectionApp.swift
+//  Shared
+//
+//  Created by Andrea Paraggio on 23/12/21.
+//
+
+import SwiftUI
+
+@main
+struct VisionContourDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
